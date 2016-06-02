a5e5fca196ba88b3fb05592309a3fa3c450aa178
Minh Dinh <dtminhpt@gmail.com>
2016-06-02 13:22:11 +0700

    For our settings screen, we need a custom view controller (SettingsViewController)

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Dinh Thi Minh on 6/2/16.
+//  Copyright © 2016 Dinh Thi Minh. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
