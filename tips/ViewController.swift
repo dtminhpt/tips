4281630aee947449b8d7f5f2efd979e62a508d73
Minh Dinh <dtminhpt@gmail.com>
2016-06-02 12:43:22 +0700

    Adding code to onEditingChanged action to allow choose tipPercentage

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -33,9 +33,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
         var billAmount = NSString(string: billField.text!).doubleValue
         
-        var tip = billAmount * 0.2
+        var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
         tipLabel.text = String(format: "$%.2f",tip)
