af5df069f7fa7b156ec8574b52d9ad3701f42dd5
Minh Dinh <dtminhpt@gmail.com>
2016-06-02 11:29:15 +0700

    Adding action to billField onEditingChanged and adding code in this action to test changing billField

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -31,6 +31,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        print("User editing bill")
+    }
 
 }
 
